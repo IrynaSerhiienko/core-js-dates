b801f74fe7466dfff1bd772d71a88edb3f0c7116
Iryna Serhiienko <irishka.sergienko@gmail.com>
2025-03-23 23:38:10 +0200

    feat: add tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -281,13 +281,15 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(period, countWorkDays, countOffDays) {
-  const startDate = new Date(period.start.split('-').reverse().join('-'));
-  const endDate = new Date(period.end.split('-').reverse().join('-'));
+// function getWorkSchedule(period, countWorkDays, countOffDays) {
+//   const startDate = new Date(period.start.split('-').reverse().join('-'));
+//   const endDate = new Date(period.end.split('-').reverse().join('-'));
 
-  const workSchedule = [];
-  let currentDate = new Date(startDate);
-}
+//   const workSchedule = [];
+//   let currentDate = new Date(startDate);
+
+//   while (currentDate <= endDate) {}
+// }
 
 /**
  * Determines whether the year in the provided date is a leap year.
